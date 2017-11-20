df869722f9c74c1c0686bda9da809a66baca30ef
Tassew <b.g.tassew@tue.nl>
2017-11-20 14:22:53 +0100

    modify voter class

--- src/voter.js
+++ src/voter.js
@@ -1,16 +1,23 @@
 
-export class Voter {
-    /**
-     * @param {HTMLElement} questions
-     */
-   
+// import Question from "./question.js";
 
-    displayQuestions(name,questions){
+ export class Voter {
+    /**
+     * @param {HTMLElement} element
+     */
+    constructor(element){
+        this.element = element;
+    }
+
+   displayQuestions(name,questions){
+
         questions.forEach(question => {
-            this.element.innerHTML = `
-            Hi ${name}! ${question} <br> `;
+            this.element.innerHTML += `
+            Hi ${name}! ${question.question} <br> 
+            ${question.answer} <br> `;
         });
     }
     
     
 }
+
