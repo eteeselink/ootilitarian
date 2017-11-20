5efbd1b30de6f7717bd0d950f54c82bc5178b6ef
truptimanik <30572066+truptimanik@users.noreply.github.com>
2017-11-20 15:41:41 +0100

    commit

--- src/q.js
+++ src/q.js
@@ -1,24 +1,23 @@
 import {Question} from "./question.js";
-   export class ques{
+   export class Q{
         constructor (element)
         {
                 this.element=element;
         }
-
-        render(name) {
+        displayQuestions(question){
+            
             this.element.innerHTML = `
             <label for="question">Question: </label><br>
-            <input type="text" name="question" id="question" value="question">
+            <input type="text" name="question" id="question" value="${question.question}">
             <br>
+           
             <label for="answer">Answer: </label><br>
-            <input type="radio" name="answer" value="yes" id="yes">
-            <label for="yes">Yes</label><br>
-
-            <input type="radio" name="answer" value="no" id="no">
-            <label for="no">No</label><br>
-            <br>
-            <button id="btn">Submit!</button>
-        `;
+             <input type="text" name="answer">
+           
+        `;   
+        }  
+        render(name) {
+          
         this.element.querySelector("button").addEventListener("click", ev => {
             // always add `preventDefault` in an event handler. otherwise, the browser
             // will do some default action which usually means submitting the data to the server, 
--- src/voter.js
+++ src/voter.js
@@ -1,5 +1,6 @@
 
-// import Question from "./question.js";
+//import Question from "./question.js";
+import {Q} from "./q.js";
 
  export class Voter {
     /**
@@ -11,8 +12,11 @@
 
     displayQuestions(questions){
         for (var i = 0; i < questions.length; i++) { 
-            this.element.innerHTML += `<br/>${questions[i].question} <br/>
-                                      ${questions[i].answer}`;
+            // this.element.innerHTML += `<br/>${questions[i].question} <br/>
+            //                           ${questions[i].answer}`;
+            var qn = new Q(this.element);
+            qn.displayQuestions(questions[i]);
+
         }        
     }   
    
